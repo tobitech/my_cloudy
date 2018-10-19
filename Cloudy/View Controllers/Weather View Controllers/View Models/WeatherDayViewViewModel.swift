aec9edb48ccbd7d9a8cd4f05343b197600469249
Oluwatobi Omotayo <tobitech@ymail.com>
2018-10-19 15:39:05 +0100

    setup a testing target for the project

---
+++ Cloudy/View Controllers/Weather View Controllers/View Models/WeatherDayViewViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  WeatherDayViewViewModel.swift
+//  Cloudy
+//
+//  Created by Oluwatobi Omotayo on 19/10/2018.
+//  Copyright © 2018 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+struct WeatherDayViewViewModel {
+    
+    let weatherDayData: WeatherDayData
+    
+    private let dayFormatter = DateFormatter()
+    private let dateFormatter = DateFormatter()
+    
+    // MARK: -
+    
+    var day: String {
+        dateFormatter.dateFormat = "EEEE"
+        return dayFormatter.string(from: weatherDayData.time)
+    }
+    
+    var date: String {
+        dateFormatter.dateFormat = "MMMM d"
+        return dateFormatter.string(from: weatherDayData.time)
+    }
+    
+    var temperature: String {
+        let min = format(temperature: weatherDayData.temperatureMin)
+        let max = format(temperature: weatherDayData.temperatureMax)
+        
+        return "\(min) - \(max)"
+    }
+    
+    private func format(temperature: Double) -> String {
+        switch UserDefaults.temperatureNotation() {
+        case .fahrenheit:
+            return String(format: "%.1f °F", temperature)
+        case .celsius:
+            return String(format: "%.1f °C", temperature.toCelcius())
+        }
+    }
+    
+    var windSpeed: String {
+        let windSpeed = weatherDayData.windSpeed
+        
+        switch UserDefaults.unitsNotation() {
+        case .imperial:
+            return String(format: "%.f MPH", windSpeed)
+        case .metric:
+            return String(format: "%.f KPH", windSpeed.toKPH())
+        }
+    }
+    
+    var image: UIImage? {
+        return UIImage.imageForIcon(withName: weatherDayData.icon)
+    }
+    
+}
+
+extension WeatherDayViewViewModel: WeatherDayRepresentable {
+    
+}
